2f3a49d0831def7a28fba007842944c489800d50
TheMonocoque <7273463+TheMonocoque@users.noreply.github.com>
2025-12-07 06:09:41 -0500

    reorganize functions to lib

--- main.py
+++ main.py
@@ -6,15 +6,9 @@ main test file
 from pydantic_settings import BaseSettings
 from dotenv import dotenv_values
 import os
-from lib import constants
 
 # import github
-from functools import partial
-from pathlib import Path
-from contextlib import contextmanager
-from random import choice
-from lib.gitobj.gitpr import GitPullRequest
-import lib.gitobj.constants as const
+import lib.shared as shd
 import lib.winners.wincombo as combowin
 import lib.mylistr.runstrtimer as rstim
 from lib.eventcache.eventcache import EventCache
@@ -28,69 +22,6 @@ class Settings(BaseSettings):
 
     class Config:
         env_file: str = "settings.env"
-
-
-def produce_synthetic_event_list() -> list[dict]:
-    """synthetic event list"""
-    cycle: list[dict] = []
-    items_to_select: list[str] = ["apple", "banana", "carrot", "dragonfruit", "elote", "fennel", "grapes"]
-    cycle.append({"name": "Scanning", "time": 50, "fuit": choice(items_to_select)})
-    for inject_time in range(60, 199):
-        cycle.append({"name": "ReportGeneration", "time": inject_time, "fruit": choice(items_to_select)})
-    cycle.append({"name": "Finished", "time": 10, "fruit": choice(items_to_select)})
-    print(f"Total synthetic events produced: {len(cycle)}")
-    return cycle
-
-
-def config_test():
-    config: dict = {}
-    config.update({"random": "relay"})
-    config["app"] = {}
-    config["app"].update({"name": "foo"})
-    print(f"{config}")
-
-
-def clone_test():
-    mydir = Path(__file__).resolve().parent
-    print(f"Directory: {mydir}")
-    print(f"{constants.git_url}")
-    print(f"{const.site_url}")
-    inc_pr = GitPullRequest(111, "main", "fix-plot-twist2")
-    inc_pr.filter = "bloomV3"
-    ret_val = inc_pr.clone()
-    print(f"{ret_val=}")
-
-    specify_prnum_ghpr: partial = partial(GitPullRequest, base_branch="main", head_branch="mybranch")
-    pr2 = specify_prnum_ghpr(123)
-    pr2.clone()
-
-    specify_prnum_branch_ghpr: partial = partial(GitPullRequest, base_branch="main")
-    pr3 = specify_prnum_branch_ghpr(123, head_branch="food")
-    pr3.clone()
-
-
-def contextrunner():
-    """test contextmanager"""
-
-    def acquire_resource(*args, **kwargs):
-        print(args, kwargs)
-        return {"message": args[0], "keys": kwargs}
-
-    def release_resource():
-        print("Releasing resource")
-
-    @contextmanager
-    def resource_one(*args, **kwargs):
-        resource = acquire_resource(*args, **kwargs)
-        try:
-            yield resource
-        finally:
-            release_resource()
-
-    with resource_one("This is an example contextrunner", trivia="jeopardy") as resone:
-        print(f"Resource ONE - {resone}")
-
-    print("Done with contextrunner")
 
 
 def main():
@@ -107,9 +38,9 @@ def main():
     }
     print(f"Quick config {config['GLOBAL_NAME']}")
 
-    config_test()
-    clone_test()
-    contextrunner()
+    shd.config_test()
+    shd.clone_test()
+    shd.contextrunner()
 
     combowin.demo_combo_choice_sample()
     rstim.run_loop_test()
@@ -122,7 +53,7 @@ def main():
     runner.report()
 
     test_event: EventCache = EventCache()
-    for event in produce_synthetic_event_list():
+    for event in shd.produce_synthetic_event_list():
         test_event.update(event["name"], event["time"])
 
     # Testing
